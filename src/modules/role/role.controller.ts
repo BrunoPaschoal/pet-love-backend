be5ae9d374218b2b363fb1bf0da9c1487bd5df62
Bruno Rocha <bruno.rocha.dev@outlook.com>
2023-01-25 19:30:24 -0300

    RolesGuard System Implementation

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -14,24 +14,29 @@ import { CreateRoleDto } from './dtos/create-role.dto';
 import { Validate } from 'class-validator';
 import { RoleEntity } from './entities/role.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from './../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('role')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Get()
+  @Roles('MASTER')
   async findAll(): Promise<RoleEntity[]> {
     return this.roleService.findAllRoles();
   }
 
   @Post()
+  @Roles('MASTER')
   @Validate(CreateRoleDto)
   async createRole(@Body() role: CreateRoleDto): Promise<RoleEntity> {
     return await this.roleService.createRole(role);
   }
 
   @Delete(':id')
+  @Roles('MASTER')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteRole(@Param('id') id: string) {
     return this.roleService.deleteRole(id);
--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -3,9 +3,10 @@ import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleEntity } from './entities/role.entity';
+import { UsersModule } from './../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoleEntity])],
+  imports: [TypeOrmModule.forFeature([RoleEntity]), UsersModule],
   controllers: [RoleController],
   providers: [RoleService],
 })
